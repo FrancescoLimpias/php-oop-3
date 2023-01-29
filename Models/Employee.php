8f41de3c76f0e692f4c03510d1f1d8779d4b3b14
Francesco Limpias <francescolimpias@gmail.com>
2023-01-29 08:04:56 +0100

    Added Emplyee class

---
+++ Models/Employee.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once __DIR__ . "/Wage.php";
+
+class Employee extends Person
+{
+
+    private Wage $wage;
+    private $employmentDate;
+
+    public function __construct($name, $last, $dateOfBirth, $placeOfBirth, $SSN, Wage $wage)
+    {
+        parent::__construct($name, $last, $dateOfBirth, $placeOfBirth, $SSN);
+        $this->setWage($wage);
+        $this->employmentDate = date("d-m-y");
+    }
+
+    public function getWage()
+    {
+        return $this->wage;
+    }
+    public function setWage(Wage $wage)
+    {
+        $this->wage = $wage;
+    }
+    public function getAnnualIncome()
+    {
+        return $this->wage->getAnnualIncome();
+    }
+
+    public function getEmploymentDate()
+    {
+        return $this->employmentDate;
+    }
+}
