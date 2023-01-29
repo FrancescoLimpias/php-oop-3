028136808496b554b60c0438fe008e8e8b4504c4
Francesco Limpias <francescolimpias@gmail.com>
2023-01-29 07:51:00 +0100

    Added Person class

---
+++ Models/Person.php
@@ -0,0 +1,70 @@
+<?php
+
+class Person
+{
+
+    private $name;
+    private $last;
+    private $dateOfBirth;
+    private $placeOfBirth;
+    private $SSN;
+
+    public function __construct(
+        $name,
+        $last,
+        $dateOfBirth,
+        $placeOfBirth,
+        $SSN
+    ) {
+        $this->setName($name);
+        $this->setLast($last);
+        $this->setDateOfBirth($dateOfBirth);
+        $this->setPlaceOfBirth($placeOfBirth);
+        $this->setSSN($SSN);
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getLast()
+    {
+        return $this->last;
+    }
+    public function setLast($last)
+    {
+        $this->last = $last;
+    }
+
+    public function getDateOfBirth()
+    {
+        return $this->dateOfBirth;
+    }
+    public function setDateOfBirth($dateOfBirth)
+    {
+        $this->dateOfBirth = $dateOfBirth;
+    }
+
+    public function getPlaceOfBirth()
+    {
+        return $this->placeOfBirth;
+    }
+    public function setPlaceOfBirth($placeOfBirth)
+    {
+        $this->placeOfBirth = $placeOfBirth;
+    }
+
+    public function getSSN()
+    {
+        return $this->SSN;
+    }
+    public function setSSN($SSN)
+    {
+        $this->SSN = $SSN;
+    }
+}
